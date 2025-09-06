6353a293d141e8cb7afb578b4f4f1643cb393a3f
Yuliya <yuliya.masalkina@outlook.com>
2025-09-06 19:13:58 +0500

    added second view controller and date picker

--- DateTimer/ViewController.swift
+++ DateTimer/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDate: UIDatePicker!
+    @IBOutlet weak var secondDate: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SecondVC" {
+            if let vc = segue.destination as? SecondViewController {
+             //   vc.time = Int(textField.text!) ?? 0
+            }
+        }
+    }
+    
 }
 
